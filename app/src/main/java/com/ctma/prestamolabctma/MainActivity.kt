b1ea5eeae32bf11a10d9e7ad2fa18357a9c8554f
FABIAN-00906 <fabianandres.marin01@gmail.com>
2026-09-02 09:44:29 -0500

    feat: integrar navegacion en actividad principal

--- app/src/main/java/com/ctma/prestamolabctma/MainActivity.kt
+++ app/src/main/java/com/ctma/prestamolabctma/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.ctma.prestamolabctma
 
-import androidx.compose.foundation.layout.padding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.ctma.prestamolabctma.ui.login.LoginScreen
+import com.ctma.prestamolabctma.navigation.AppNavigation
 import com.ctma.prestamolabctma.ui.theme.PrestamoLabCTMATheme
 import com.ctma.prestamolabctma.viewmodel.LoginViewModel
 
@@ -25,17 +21,9 @@ class MainActivity : ComponentActivity() {
 
                 val loginViewModel: LoginViewModel = viewModel()
 
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-
-                    LoginScreen(
-                        loginViewModel = loginViewModel,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    )
-                }
+                AppNavigation(
+                    loginViewModel = loginViewModel
+                )
             }
         }
     }
